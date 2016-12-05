2ac2d6d7ce202bf08d4e1a27f6c0afb2743ba5b8
CSE\dxz149 <dxz149@psu.edu>
2016-12-05 17:54:43 -0500

    pbs file for Abilene

---
+++ Backup/Backup/Backup/Tomo_Abilene.sh
@@ -0,0 +1,2 @@
+\python multiple_Tomography.py -1 Abilene 4 unknown_capacity diman 0 0 6 1.0 0.5 0  > out.txt 2> error.txt
+
---
+++ Backup/Backup/Backup/Tomo_Bell.sh
@@ -0,0 +1,2 @@
+\python multiple_Tomography.py -1 BellCanada_Random_Capacity 4 unknown_capacity diman 0 0 10 1.0 0.4 0  > out.txt 2> error.txt
+
