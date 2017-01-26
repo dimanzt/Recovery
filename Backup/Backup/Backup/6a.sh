ee3d959d4e15e9c603e269c2cf25d4d8148a2b90
CSE\dxz149 <dxz149@psu.edu>
2017-01-26 17:11:05 -0500

    Adding the script files for Figures

---
+++ Backup/Backup/Backup/6a.sh
@@ -0,0 +1,3 @@
+\python multiple_simulations_pairs.py -1 BellCanada_Random_Capacity_High_Capacity_2_backbone_eterogeneous 2 unknown_capacity diman 0 0 100 1.0 0 0.001 0 > out.txt 2> error.txt
+
+
---
+++ Backup/Backup/Backup/6a_Delta.sh
@@ -0,0 +1,3 @@
+\python multiple_simulations_Delta_pairs.py -1 DeltaCom_Connettivity_Random_Capacity 2 unknown_capacity diman 0 0 20 1.0 0 0.001 0 > out.txt 2> error.txt
+
+
